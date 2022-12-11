6bb7953d8419d4355145ac9e7ad04c9d9b35e92a
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-11 02:37:47 +0100

    working with last bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as tools from './tools';
 import './App.scss';
 
 const _originalNames = ['Marco', 'Luka', 'Jonas', 'Lena', 'Emma', 'Leah'];
 
+const phases: any = {
+	haventClickedYet: {
+		buttonName: 'Select'
+	},
+	selectingNames: {
+		buttonName: 'Select'	
+	},
+	onlyOnePersonLeft: {
+		buttonName: 'Finish'
+	},
+	finished: {
+		buttonName: 'RESET'
+	}
+}
+
 function App() {
 	const [availableNames, setAvailableNames] = useState(_originalNames);
 	const [currentName, setCurrentName] = useState('');
-	const [selectedNames, setSelectedNames] = useState([]);
+	const [selectedNames, setSelectedNames] = useState<string[]>([]);
+	const [currentPhase, setCurrentPhase] = useState('haventClickedYet');
 
 	const handleMainButton = () => {
+		if (currentName !== '') {
+			selectedNames.push(currentName);
+		}
 		const _currentName = tools.removeRandomItemFromArray(availableNames);
-		setCurrentName(_currentName);
+		setAvailableNames([...availableNames]);
+		setCurrentName(_currentName === null ? '' : _currentName);
 	};
+
+	useEffect(() => {
+		if (availableNames.length === 0) {
+			setCurrentPhase('onlyOnePersonLeft');
+		}
+	}, [availableNames])
+
+	useEffect(() => {
+		if (currentName !== '' && selectedNames.length === 0) {
+			setAvailableNames(_originalNames);
+			setSelectedNames([]);
+			setCurrentPhase('haventClickedYet');
+		}
+		if (availableNames.length === 0 && currentName === '') {
+			setCurrentPhase('finished')
+		}
+	}, [currentName]);
 
 	return (
 		<div className="App">
@@ -27,9 +64,7 @@ function App() {
 				})}
 			</div>
 			<div className="currentArea">
-				<button className="buttonSelect" onClick={handleMainButton}>
-					Select
-				</button>
+				<button className="buttonSelect" onClick={handleMainButton}>{phases[currentPhase].buttonName}</button>
 				{currentName && (
 					<div className="currentName">{currentName}</div>
 				)}
